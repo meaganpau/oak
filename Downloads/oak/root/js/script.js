afc458eba04fa7363941dfefc53c6b08de37d8f6
Meagan Pau <meaganpau@Meagans-MacBook-Pro-2.local>
2016-08-25 01:21:47 -0400

    .

---
+++ Downloads/oak/root/js/script.js
@@ -0,0 +1,18 @@
+$(document).ready( function(){
+  console.log('sipsupsupsu');
+
+    $('#click').click( function(event){
+        
+        event.stopPropagation();
+        
+        $('#dropDown').toggle();
+        
+    });
+    
+    $(document).click( function(){
+
+        $('#dropDown').hide();
+
+    });
+
+});
